3c03fa507de436c93017283bbda37fdb1ee178f9
kbarszczak <kamilbarszczak62@gmail.com>
2023-05-05 16:32:46 +0200

    Change me section

--- scripts/switches.js
+++
@@ -1,8 +0,0 @@
-let root = document.querySelector(':root')
-let langSwitch = document.querySelector("#lang-switch")
-
-langSwitch.addEventListener('click', changeLanguage)
-
-function changeLanguage(event){
-    
-}
