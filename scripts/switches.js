d3c71c06e8da4bc2864ac1006b515baf17256de6
kbarszczak <kamilbarszczak62@gmail.com>
2023-05-05 15:53:21 +0200

    Added animated background to slider

---
+++ scripts/switches.js
@@ -0,0 +1,8 @@
+let root = document.querySelector(':root')
+let langSwitch = document.querySelector("#lang-switch")
+
+langSwitch.addEventListener('click', changeLanguage)
+
+function changeLanguage(event){
+    
+}
